1daab6de9541d69e8c8107f0a5109853bd6fd5e2
Santhos <santhoshsunthar@gmail.com>
2021-09-23 23:04:46 +0530

    day01

---
+++ Day01.js
@@ -0,0 +1,20 @@
+// Like traditional hello world printing
+
+console.log("Hello Friends! Welcome to my GitHub Repository!");
+
+
+
+
+// Comments
+
+var name; // Declared a variable called name
+
+
+/* Declared a variable called number 
+and 
+assigned a value  */
+
+var number = 1;
+
+
+
