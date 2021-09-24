ba7ca2516fe41f750fb27e1a2146509c475eae2d
Santhos <santhoshsunthar@gmail.com>
2021-09-24 22:49:48 +0530

    day02

---
+++ Day02.js
@@ -0,0 +1,39 @@
+// Variable declarations and initializing
+
+var name; // Declared a variable called name
+
+var number; // Declared a variable called number
+
+name = "JavaScript"; // Assigned a value for variable name
+
+console.log(name); // Output >>> JavaScript
+
+name = "JS"; // Assigned a value again for variable name
+
+console.log(name); // Output >>> JS
+
+console.log(number); // Output >>> undefined
+
+number = 1; // Assigned a value for variable number
+
+console.log(number); // Output >>> 1
+
+var firstName = "firstName"; // Variable initialization when declared
+
+let newName = "NewJS"; // Variable declared using let to use the variable within a scope
+
+console.log(newName); // Output >>> NewJS
+
+const value = 11211; // Using const to create constant values
+
+console.log(value); // Output >>> 11211
+
+// Case sensitive or not?
+
+var A = "First Value"; // Initialize a variable A with value 1
+
+var a = "Second Value"; // Initialize a variable a with value 2
+
+console.log(A); // Output >>> First Value
+
+console.log(a); // Output >>> Second Value
