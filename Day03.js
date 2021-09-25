26a352c95e85dc3876e5563c3549111da2c559b4
Santhos <santhoshsunthar@gmail.com>
2021-09-25 23:26:01 +0530

    day03

---
+++ Day03.js
@@ -0,0 +1,76 @@
+// Arithmatic operations
+
+// Addition
+
+var sum = 1 + 2;
+
+console.log(sum); // Output >>> 3
+
+// Difference
+
+difference = 2 - 1;
+
+console.log(difference); // Output >>> 1
+
+// Multiplication
+
+var multiply = 3 * 3;
+
+console.log(multiply); // Output >>> 9
+
+var product = 1.0 * 2.0;
+
+console.log(product); // Output >>> 2
+
+product = 1.0 * 2.2;
+
+console.log(product); // Output >>> 2.2
+
+// Division
+
+var division = 9 / 3; 
+
+console.log(division); // Output >>> 3
+
+division = 0.1 / 0.2;
+
+console.log(division); // Output >>> 0.5
+
+division = 2.0 / 1.0;
+
+console.log(division); // Output >>> 2
+
+// Reminders
+
+var reminder = 10 % 3;
+
+console.log(reminder); // Output >>> 1
+
+
+// Compound assignment with augmented addtion, difference, multiplication and division
+
+var a = 1, b = 2, c = 3, d = 4;
+
+a += 2;
+b -= 3;
+c *= 4;
+d /= 5;
+
+console.log(a, b, c, d); // Output >>> 3 -1 12 0.8
+
+
+// Increments
+
+var inc = 1;
+
+inc++; // inc = inc + 1;
+
+console.log(inc); // Output >>> 2
+
+// Decrements
+
+var dec = 2; 
+
+dec--; // dec = dec - 1;
+
+console.log(dec); // Output >>> 1
